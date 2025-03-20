cf3ed0e7e69d67a5f03f0616452781926d54cb1f
ilya li <101355829+vsdifficult@users.noreply.github.com>
2025-03-20 09:06:43 +0300

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,6 +1,6 @@
 # Option 1: Use Docker Hub (will prompt for login)
 chmod +x run.sh
-./run.sh --registry ilyavs46578
+./run.sh --registry dockerhubusername
 
 # Option 2: Skip Docker Hub push (use local images only)
 ./run.sh --local-registry --skip-terraform
